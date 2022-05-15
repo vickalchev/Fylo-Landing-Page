a3f6d65e4a1c0c0cd305b034aa01af7643fea502
Vic Kalchev <vic@pivotup.co>
2022-05-15 17:06:11 -0400

    restaurant order

---
+++ restaurant-orders/index.ts
@@ -0,0 +1,12 @@
+import { restaurants, Restaurant } from "./restaurants";
+import { orders, Order, PriceBracket } from "./orders";
+
+/// Add your getMaxPrice() function below:
+
+/// Add your getOrders() function below:
+
+/// Add your printOrders() function below:
+
+/// Main
+// const elligibleOrders = getOrders(PriceBracket.Low, orders);
+// printOrders(restaurants, elligibleOrders);
